f9a7984e716a3399f34c1f5bc29c0b8006ffc5e9
anthony <anthonymorlett@gmail.com>
2021-09-10 10:36:17 -0500

    fix nav bar
    try to fix nav bar scroll down with a extra div

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,9 @@ const IndexPage = () => {
         }}
       >
       <Hero id="Home" className="position-relative pt-22 pt-lg-31 pb-13 pb-lg-25" />
+      
         <Testimonial  id="Clients" className="bg-default-5 pt-13 pt-md-18 pt-lg-24 pb-9 pb-md-13 pb-lg-28" />
+            <div id="Portfolio"> </div>
         <Content3  id="Thomas" className="bg-dark-cloud pt-15 pt-md-19 pt-lg-33 pt-xl-0 pb-15 pb-md-19 pb-lg-31 pb-xl-0 position-relative overflow-hidden" />
         <Content1  id="Tylt" className="bg-default-5 pt-13 pt-md-18 pt-lg-24 pb-9 pb-md-13 pb-lg-28" />
         <Content2  id="Agent" className="bg-dark-cloud pt-15 pt-md-19 pt-lg-33 pt-xl-0 pb-15 pb-md-19 pb-lg-31 pb-xl-0 position-relative overflow-hidden" />
