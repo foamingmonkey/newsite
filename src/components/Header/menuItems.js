36edcfb2e930d7d07fd2d21e26de9b8407b43136
anthony <anthonymorlett@gmail.com>
2022-05-04 14:58:27 -0500

    Update menuItems.js

--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -1,46 +1,43 @@
 export const menuItems = [
-         {
-           name: "Home",
-           label: "Home",
-           
-         },
-            {
-           name: "Clients",
-           label: "Clients",
-           
-         },
-         {
-           name: "Portfolio",
-           label: "Portfolio",
-           items: [
-             { name: "Thomas", label: "Thomas Jane" },
-             {
-               name: "Tylt",
-               label: "Tylt",
-              
-             },
-             {
-               name: "Agent",
-               label: "Agent Image",
-              
-             },
+  {
+    name: "Home",
+    label: "Home",
+  },
+  {
+    name: "Clients",
+    label: "Clients",
+  },
+  {
+    name: "Portfolio",
+    label: "Portfolio",
+    items: [
+      { name: "Thomas", label: "Thomas Jane" },
+      {
+        name: "Tylt",
+        label: "Tylt",
+      },
+      {
+        name: "Agent",
+        label: "Agent Image",
+      },
 
-              {
-               name: "PCA",
-               label: "Pca Agency",
-              
-             },
-                  {
-               name: "Zoggs",
-               label: "Zoggs USA",
-              
-             },
-                {
-               name: "ICF",
-               label: "ICF International",
-              
-             },
-           ],
-         },
-         
-       ];
+      {
+        name: "PCA",
+        label: "Pca Agency",
+      },
+      {
+        name: "Zoggs",
+        label: "Zoggs USA",
+      },
+      {
+        name: "ICF",
+        label: "ICF International",
+      },
+    ],
+  },
+  {
+    name: "https://github.com/anthonymBD",
+    label: "Github",
+    isExternal: true,
+  },
+];
