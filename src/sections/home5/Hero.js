40789115e353daeaafc467ef690fa3ad4579a8ab
anthony <anthonymorlett@gmail.com>
2022-05-06 17:11:49 -0500

    Update Hero.js

--- src/sections/home5/Hero.js
+++ src/sections/home5/Hero.js
@@ -18,9 +18,62 @@ const Hero = ({ className, ...rest }) => {
               data-aos-delay={500}
             >
               <h1 className="font-size-12 mb-9">Anthony Morlett</h1>
-              <p className="font-size-8 mb-lg-13 pr-sm-15 pr-lg-8 pr-xl-15">
-               Web Design, Shopify Development, &amp; Front End
+                  <h5 className="font-size-8 mb-lg-13 pr-sm-15 pr-lg-8 pr-xl-15 text-dodger-blue-1">
+                Web Design, Shopify &amp; Front End Development
+              </h5>
+              <p>
+                I am an experienced UX/Web designer and front-end developer who
+                is passionate about communication and digital solutions. I am a
+                self-starter with a strong background in user experience and
+                visual design and a proactive problem-solver who understands
+                business needs and takes full responsibility for the entire
+                process from problem definition to solution. When it comes to
+                solving complex challenges, I take a roll-up-your-sleeves,
+                entrepreneurial approach. In offering recommendations, I display
+                professionalism and confidence as both an individual worker and
+                a team player. I am service-oriented, flexible, friendly, and
+                thorough. I am Shopify and E-commerce expert. Social media
+                marketing, Amazon FBA, Adobe Creative Suite, Axure, and creative
+                direction are some of my other skills.
               </p>
+
+ <div class="d-flex align-items-center py-6 mr-xl-15">
+              <div class="mr-7">
+                <a
+                  class="bg-sunset text-white circle-50 video-btn"
+                  href="https://docs.google.com/document/d/1hlnVEf9AyHZ4a9_HPxRDuWzpqtdke_MN1MIfM0acLYU/edit?usp=sharing"
+                >
+                  <i class="fas fa-file-pdf"></i>
+                </a>
+              </div>
+              <div class="mt-0">
+                <a
+                  class="font-size-6 mb-0 text-dodger-blue-1"
+                  href="https://docs.google.com/document/d/1hlnVEf9AyHZ4a9_HPxRDuWzpqtdke_MN1MIfM0acLYU/edit?usp=sharing"
+                >
+                  View My Resume
+                </a>
+              </div>
+
+              <div class="mx-7">
+                <a
+                  class="bg-sunset text-white circle-50 video-btn"
+                  id="Thomas"
+                  href="#Thomas"
+                >
+                  <i class="fas fa-solid fa-bolt"></i>
+                </a>
+              </div>
+              <div class="mt-0">
+                <a
+                  href="#Thomas"
+                  id="Thomas"
+                  class="font-size-6 mb-0 text-dodger-blue-1"
+                >
+                  View My Portfolio
+                </a>
+              </div>
+            </div>
            
             </div>
           </div>
